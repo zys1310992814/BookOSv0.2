4b0b65f3f2a0ed5fb03592e3554c6c7bbb9370ee
Eric h <2323168280@qq.com>
2019-02-24 21:54:06 +0800

    Add files via upload

--- src/include/stdint.h
+++ src/include/stdint.h
@@ -9,13 +9,15 @@ typedef          short int16_t;
 typedef unsigned char  uint8_t;
 typedef          char  int8_t;
 
-typedef unsigned int   u32;
+typedef unsigned int   u64;
+typedef unsigned long   u32;
 typedef          int   i32;
 typedef unsigned short u16;
 typedef          short i16;
 typedef unsigned char  u8;
 typedef          char  i8;
 
+typedef unsigned long  uint64;
 typedef unsigned int   uint32;
 typedef          int   int32;
 typedef unsigned short uint16;
