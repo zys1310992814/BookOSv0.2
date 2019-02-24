ae92c28610d55d1583686abc29082f76321dd062
Eric h <2323168280@qq.com>
2019-02-24 21:54:36 +0800

    Add files via upload

--- src/include/sys/pipe.h
+++ src/include/sys/pipe.h
@@ -1,12 +1,14 @@
-#ifndef _PIPE_
-#define _PIPE_
+#ifndef _LIB_PIPE_
+#define _LIB_PIPE_
+
 #include <stdint.h>
 #include <sys/memory.h>
 #include <sys/sync.h>
 #include <string.h>
-void sys_init_pipe();
+void init_pipe();
 uint64_t sys_new_pipe(uint32_t size);
 bool sys_write_pipe(uint64_t pipe_id,void* data,uint32_t size);
 bool sys_read_pipe(uint64_t pipe_id,void* buffer);
 bool sys_close_pipe(uint64_t pipe_id);
+
 #endif
